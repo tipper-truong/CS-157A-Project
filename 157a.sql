643f5fa05a8229d4326ba1292d62b8bf1fa311ef
Tipper Truong <tipper.truong.milo@gmail.com>
2017-11-26 13:49:57 -0800

    Parse sql file to add 15 random data entries for Book Database

---
+++ 157a.sql
@@ -0,0 +1,72 @@
+INSERT INTO Titles (isbn, title, editionNumber, year, publisherID, price) 
+VALUES
+	("0984782869", "Cracking the Coding Interview: 189 Programming Questions and Solutions", 6, "2015", 1, 34.89),
+	("1118431111", "Big Java Early Objects", 5, "2013", 2, 132.77),
+	("0439064872", "Harry Potter and the Chamber of Secrets" , 1, "2000", 3, 6.79),
+	("0553593714", "A Game of Thrones" , 1, "2011", 4, 6.29),
+	("1449355730" , "Learning Python" , 5, "2013", 5, 58.58),
+	("0134706056" , "Android Programming: The Big Nerd Ranch Guide" , 3, "2017", 6, 31.15),
+	("054792819X", "Lord of the Rings: Return of the King" , 1, "2012", 7, 10.48),
+	("1118531647" , "JavaScript and JQuery: Interactive Front-End Web Development" , 1, "2014", 8, 24.01),
+	("0132350882" , "Clean Code: A Handbook of Agile Software Craftsmanship" , 1, "2008", 9, 28.66),
+	("0201485672", "Refactoring: Improving the Design of Existing Code", 1, "1999", 10, 44.33),
+	("0131177052", "Working Effectively with Legacy Code", 1, 2004, 11, 60.46),
+	("1974492559", "Confessions of a Software Techie: The Surprising Truth about Things that Really Matter", 1, 2017, 12, 5.99),
+	("0385484518", "Tuesdays with Morrie: An Old Man, a Young Man, and Life's Greatest Lesson", 1, 2007, 13, 11.99),
+	("B0015DROBO", "The Girl with a Dragon Tattoo", 1, "2008", 14, 9.99),
+	("140882485X", "The Kite Runner", 1, 2004, 15, 11.99);
+
+INSERT INTO Author (authorID, firstName, lastName) 
+VALUES
+	(1,"Gayle","Laakmann McDowell"),
+	(2, "Cay", "Horstmann"),
+	(3, "JK" ,"Rowling"),
+	(4 , "George R. R." , "Martin"),
+	(5 , "Mark" , "Lutz"),
+	(6 , "Chris" , "Stewart"),
+	(7, "J.R.R.", "Tolkien"),
+	(8 , "Jon" , "Duckett"),
+	(9, "Robert C.", "Martin"),
+	(10, "Kent", "Beck"), 
+	(11, "Michael", "Feathers"),
+	(12, "Ramakrishna", "Reddy"),
+	(13, "Mitch", "Albom"),
+	(14, "Stieg", "Larsson"),
+	(15, "Khaled", "Hosseini"); 
+
+INSERT INTO AuthorISBN (authorID, isbn) 
+VALUES
+	(1, "0984782869"),
+	(2, "1118431111"),
+	(3, "0439064872"),
+	(4, "0553593714"),
+	(5, "1449355730"),
+	(6, "0134706056"),
+	(7, "054792819X"),
+	(8 , "1118531647"),
+	(9, "0132350882"),
+	(10, "0201485672"),
+	(11, "0131177052"),
+	(12, "1974492559"),
+	(13, "0385484518"),
+	(14, "B0015DROBO"),
+	(15, "140882485X"); 
+	
+INSERT INTO Publisher(publisherID, publisherName) 
+VALUES
+	(1, "CareerCup"),
+	(2, "Wiley"),
+	(3, "Scholastic"),
+	(4, "Bantam"),
+	(5 ,"O'Reilly Media"),
+	(6, "Big Nerd Ranch Guides"),
+	(7, "Mariner Books"),
+	(8 , "Wiley"),
+	(9 , "Prentice Hall"),
+	(10, "Addison-Wesley Professional"),
+	(11, "Prentice Hall"),
+	(12, "PublicSpeakKing"),
+	(13, "Broadway Books"),
+	(14, "Vintage Crime"),
+	(15, "Penguin Group");
+
--- Database.java
+++ Database.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.Scanner;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 /**
  * Database manipulation and operations for Titles, Author, Publisher, and Author ISBN
  * @author CS 157A Group #3
@@ -9,48 +15,47 @@ public class Database {
 	private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String DB_URL_INITIAL =  "jdbc:mysql://localhost:3306/?autoReconnect=true&useSSL=false"; // use when Books database has not been created
 	private static final String DB_URL =  "jdbc:mysql://localhost:3306/Books?autoReconnect=true&useSSL=false"; // use when Books database has been created
-	
+
 	// Database Name
 	private final String DB_NAME = "Books";
-	
+
 	// Database Table Names;
 	private final String TITLES_TABLE = "Titles";
 	private final String AUTHOR_TABLE = "Author";
 	private final String PUBLISHER_TABLE = "Publisher";
 	private final String AUTHOR_ISBN_TABLE = "AuthorISBN";
-	
+
 	// Database Table Creation Queries
 	private final String CREATE_TITLES_QUERY = "CREATE TABLE Titles " + 
-											"("
-											+ "isbn CHAR(10) not NULL, " 
-											+ "title VARCHAR(500) not NULL, "
-											+ "editionNumber INTEGER not NULL, "
-											+ "year CHAR(4) not NULL, " 
-											+ "publisherID INTEGER not NULL, "
-											+ "price FLOAT not NULL, "
-											+ "PRIMARY KEY ( isbn )"
-											+ ")";
-	
+			"("
+			+ "isbn CHAR(10) not NULL, " 
+			+ "title VARCHAR(500) not NULL, "
+			+ "editionNumber INTEGER not NULL, "
+			+ "year CHAR(4) not NULL, " 
+			+ "publisherID INTEGER not NULL, "
+			+ "price FLOAT not NULL, "
+			+ "PRIMARY KEY ( isbn )"
+			+ ")";
+
 	private final String CREATE_AUTHOR_QUERY = "CREATE TABLE Author " + 
-											"("
-											+ "authorID INTEGER not NULL, "
-											+ "firstName CHAR(20) not NULL, "
-											+ "lastName CHAR(20) not NULL, "
-											+ " PRIMARY KEY ( authorID )"
-											+ ")";
-	
+			"("
+			+ "authorID INTEGER not NULL, "
+			+ "firstName CHAR(20) not NULL, "
+			+ "lastName CHAR(20) not NULL, "
+			+ " PRIMARY KEY ( authorID )"
+			+ ")";
+
 	private final String CREATE_PUBLISHER_QUERY = "CREATE TABLE Publisher " + 
-												"("
-												+ "publisherID INTEGER not NULL, "
-												+ "publisherName CHAR(100) not NULL"
-												+ ")";
-	
+			"("
+			+ "publisherID INTEGER not NULL, "
+			+ "publisherName CHAR(100) not NULL"
+			+ ")";
+
 	private final String CREATE_AUTHOR_ISBN_QUERY = "CREATE TABLE AuthorISBN " + 
-													"("
-													+ "authorID INTEGER not NULL, "
-													+ "isbn CHAR(10) not NULL"
-													+ ")";
-											
+			"("
+			+ "authorID INTEGER not NULL, "
+			+ "isbn CHAR(10) not NULL"
+			+ ")";
 
 	// Database credentials
 	private final String USERNAME = "root";
@@ -74,25 +79,29 @@ public class Database {
 		System.out.println("Connecting to database...");
 		dbConnect = DriverManager.getConnection(DB_URL_INITIAL, USERNAME, PASSWORD);
 		boolean dbExist = checkDatabaseExists(DB_NAME);
-		
+
 		// Create Books Database
 		createBooksDatabase(dbExist); 
-		
+
 		// Assuming Books Database has been created, create a new connection
 		dbConnect = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 
 		// Create Tables Titles, Author, Publisher, AuthorISBN
 		boolean tableExists = checkTableExists(TITLES_TABLE);
 		createTable(tableExists, TITLES_TABLE, CREATE_TITLES_QUERY);
-		
+
 		tableExists = checkTableExists(AUTHOR_TABLE);
 		createTable(tableExists, AUTHOR_TABLE, CREATE_AUTHOR_QUERY);
-		
+
 		tableExists = checkTableExists(PUBLISHER_TABLE);
 		createTable(tableExists, PUBLISHER_TABLE, CREATE_PUBLISHER_QUERY);
-		
+
 		tableExists = checkTableExists(AUTHOR_ISBN_TABLE);
 		createTable(tableExists, AUTHOR_ISBN_TABLE, CREATE_AUTHOR_ISBN_QUERY);
+		
+		// Parse 157a.sql file and insert 15 entries for each table in Books database
+		File inputFile = new File("src/157a.sql");
+		executeSqlScript(dbConnect, inputFile);
 
 	}
 
@@ -104,9 +113,9 @@ public class Database {
 	{
 		dbConnect.close();
 	}
-	
+
 	/*********************************	Database Helper Functions	***************************************/
-	
+
 	/**
 	 * Responsible for creating the database. If it doesn't exist, create Books database. Else notify user that database is already created
 	 * @param dbExist
@@ -126,10 +135,10 @@ public class Database {
 			// Else Books database exists
 			System.out.println("Database: " + DB_NAME + " already exists");
 		}
-		
-		
+
+
 	}
-	
+
 	/**
 	 * Creates a table for Books Database
 	 * @param tableExists Check if table exists or not
@@ -147,7 +156,7 @@ public class Database {
 		} else {
 			System.out.println("Table: " + tableName + " already exists");
 		}
-		
+
 	}
 
 	/**
@@ -169,7 +178,7 @@ public class Database {
 		}
 		return dbExists;
 	}
-	
+
 	/**
 	 * Checks if table exists
 	 * @param tableName The table name to we want to check if it exists
@@ -188,4 +197,61 @@ public class Database {
 			}
 		} return tableExists;
 	}
+	
+	/** 
+	 * Code Citation: https://stackoverflow.com/questions/1497569/how-to-execute-sql-script-file-using-jdbc
+	 * Responsible for executing sql statements from a .sql file
+	 * @param conn Database Connection
+	 * @param inputFile The .sql file you want to parse through
+	**/
+	public void executeSqlScript(Connection conn, File inputFile) {
+
+		// Delimiter
+		String delimiter = ";";
+
+		// Create scanner
+		Scanner scanner;
+		try {
+			scanner = new Scanner(inputFile).useDelimiter(delimiter);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		// Loop through the SQL file statements 
+		Statement currentStatement = null;
+		while(scanner.hasNext()) {
+
+			// Get statement 
+			String rawStatement = scanner.next() + delimiter;
+			try {
+				// Execute statement
+				currentStatement = conn.createStatement();
+				try {
+					try {
+						currentStatement.execute(rawStatement); 
+					} catch (MySQLIntegrityConstraintViolationException e) {
+						System.out.println("Values already added to Books Database");
+					}
+				} catch (MySQLSyntaxErrorException e) {
+					System.out.println("Values added to Books Database successfully");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// Release resources
+				if (currentStatement != null) {
+					try {
+						currentStatement.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				currentStatement = null;
+			}
+		}
+
+		scanner.close();
+	}
 }
+
